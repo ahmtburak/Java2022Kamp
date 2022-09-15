cad11c554d62504fec9792113267ede3edf05a23
ahmtburak <matrixkrista1@gmail.com>
2022-09-15 16:32:33 +0300

    Değişkenler

---
+++ javaHelloWorld/src/variables/Main.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci Sayisi: ";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+
+
+	}
+
+}
