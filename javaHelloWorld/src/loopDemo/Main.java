49f976b517f5189176e77c63f9f267bb33a7c8d6
ahmtburak <matrixkrista1@gmail.com>
2022-09-15 20:45:57 +0300

    loopDemoWhile

--- javaHelloWorld/src/loopDemo/Main.java
+++ javaHelloWorld/src/loopDemo/Main.java
@@ -5,10 +5,20 @@ import java.util.Iterator;
 public class Main {
 
 	public static void main(String[] args) {
+		//for
 		for (int i = 2; i < 10; i+=2) {
 			System.out.println(i);
 		}
 		System.out.println("Döngü bitti");
+		
+		int i = 2;
+		//While
+		while(i<10) {
+			System.out.println(i);
+			i+=2;
+		}
+		
+		System.out.println("While döngüsü bitti");
 	}
 
 }
