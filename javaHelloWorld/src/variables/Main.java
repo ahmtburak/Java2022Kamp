b1e075bdfc12c4fed1367701415b424ba7a4d114
ahmtburak <matrixkrista1@gmail.com>
2022-09-15 16:34:01 +0300

    D

--- javaHelloWorld/src/variables/Main.java
+++ javaHelloWorld/src/variables/Main.java
@@ -3,7 +3,7 @@ package variables;
 public class Main {
 
 	public static void main(String[] args) {
-		int ogrenciSayisi = 12;
+		int ogrenciSayisi = 11;
 		String mesaj = "Öğrenci Sayisi: ";
 		
 		System.out.println(mesaj + ogrenciSayisi);
