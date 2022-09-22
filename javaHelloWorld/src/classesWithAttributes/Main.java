a8398b55b5d02e2dda08aa67b7de67fd9276a1e7
ahmtburak <matrixkrista1@gmail.com>
2022-09-22 18:12:13 +0300

    constructors

--- javaHelloWorld/src/classesWithAttributes/Main.java
+++ javaHelloWorld/src/classesWithAttributes/Main.java
@@ -3,17 +3,17 @@ package classesWithAttributes;
 public class Main {
 
 	public static void main(String[] args) {
-		Product product = new Product();
-		product.name = "Laptop";
-		product.id = 1;
-		product.description = "Asus laptop";
-		product.price = 5000;
-		product.stockAmount = 3;
+		Product product = new Product(1,"Laptop","Asus Laptop",3000,2,"Siyah");
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus laptop");
+		product.setPrice(5000);
+		product.setStockAmount(3);
 		
 		ProductManager productManager = new ProductManager();
 		productManager.Add(product);
 		
-		System.out.println(product.name);
+		System.out.println(product.getKod());
 
 	}
 
