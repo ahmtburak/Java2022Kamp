502c9e71acc06a0d4695aeecce3834cb3fbf4116
ahmtburak <matrixkrista1@gmail.com>
2022-09-22 16:08:18 +0300

    Variable arguments

--- javaHelloWorld/src/methods2/Main.java
+++ javaHelloWorld/src/methods2/Main.java
@@ -8,6 +8,8 @@ public class Main {
 		System.out.println(yeniMesaj);
 		int sayi = topla(15,7);
 		System.out.println(sayi);
+		int toplam = topla2(1,2,3,4,5,6);
+		System.out.println(toplam);
 
 	}
 	
@@ -25,6 +27,14 @@ public class Main {
 	
 	public static int topla(int sayi1, int sayi2) {
 		return sayi1+sayi2;
+	}
+	
+	public static int topla2(int... sayilar) {
+		int toplam = 0;
+		for (int sayi : sayilar) {
+			toplam+=sayi;
+		}
+		return toplam;
 	}
 	
 	public static String sehirVer() {
